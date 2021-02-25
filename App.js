f15feb8d25f361422865f97913595a94cb9f3f30
eugene-tsarev <eugene.tsarev@gmail.com>
2021-02-25 15:17:23 +0500

    Fist commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Navbar} from './src/Navbar'
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Navbar title='Приложение' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
